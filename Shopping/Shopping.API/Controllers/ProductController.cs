813752d6b6bb698cc2a92ced87412346a499076f
erncab <erncab@users.noreply.github.com>
2021-05-18 15:10:21 -0400

    Added ProductController::Get() HttpGet method. Copy the Data folder from Shopping.client to Shopping.API.

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shopping.API.Models;
+
+namespace Shopping.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController
+    {
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            var rng = new Random();
+
+            return Enumerable.Range(1, 5).Select(index => new Product
+            {
+                Name = "ABC"
+            }).ToArray();
+        }
+    }
+}
